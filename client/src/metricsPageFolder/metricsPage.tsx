50ddfba347a88308ee705d092228bbee96b67c97
Pgerardocastaneda <68565812+Pgerardocastaneda@users.noreply.github.com>
2024-11-24 16:19:38 -0800

    metrics tracker is now a pop up

--- client/src/metricsPageFolder/metricsPage.tsx
+++ client/src/metricsPageFolder/metricsPage.tsx
@@ -1,43 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './metricsPage.css';
 
 const MetricsPage = () => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
   return (
-    <div className="metrics-container">
-      <h1>Metrics</h1>
-      <p>Date: {new Date().toLocaleDateString()}</p>
-      <form className="metrics-form">
-        <div className="metrics-input-group">
-          <label htmlFor="weight">Weight</label>
-          <input type="number" id="weight" placeholder="Value" />
+
+
+    <div className="metrics-pop-up">
+      <button className="metrics-button" onClick={openModal}>
+        Track Metrics
+      </button>
+
+      {isModalOpen &&( 
+
+      <div className="overlay">
+
+        <div className="metrics-container">
+          <h1>Metrics</h1>
+          <p>Date: {new Date().toLocaleDateString()}</p>
+          <form className="metrics-form">
+            <div className="metrics-input-group">
+              <label htmlFor="weight">Weight</label>
+              <input type="number" id="weight" placeholder="Value" />
+            </div>
+            <div className="metrics-input-group">
+              <label htmlFor="shoulders">Shoulders</label>
+              <input type="number" id="shoulders" placeholder="Value" />
+            </div>
+            <div className="metrics-input-group">
+              <label htmlFor="chest">Chest</label>
+              <input type="number" id="chest" placeholder="Value" />
+            </div>
+            <div className="metrics-input-group">
+              <label htmlFor="waist">Waist</label>
+              <input type="number" id="waist" placeholder="Value" />
+            </div>
+            <div className="metrics-input-group">
+              <label htmlFor="glutes">Glutes</label>
+              <input type="number" id="glutes" placeholder="Value" />
+            </div>
+            <div className="metrics-input-group">
+              <label htmlFor="right-thigh">Right Thigh</label>
+              <input type="number" id="right-thigh" placeholder="Value" />
+            </div>
+            <div className="metrics-input-group">
+              <label htmlFor="left-thigh">Left Thigh</label>
+              <input type="number" id="left-thigh" placeholder="Value" />
+            </div>
+            <button type="submit" className="submit-button">Submit</button>
+          </form>
         </div>
-        <div className="metrics-input-group">
-          <label htmlFor="shoulders">Shoulders</label>
-          <input type="number" id="shoulders" placeholder="Value" />
-        </div>
-        <div className="metrics-input-group">
-          <label htmlFor="chest">Chest</label>
-          <input type="number" id="chest" placeholder="Value" />
-        </div>
-        <div className="metrics-input-group">
-          <label htmlFor="waist">Waist</label>
-          <input type="number" id="waist" placeholder="Value" />
-        </div>
-        <div className="metrics-input-group">
-          <label htmlFor="glutes">Glutes</label>
-          <input type="number" id="glutes" placeholder="Value" />
-        </div>
-        <div className="metrics-input-group">
-          <label htmlFor="right-thigh">Right Thigh</label>
-          <input type="number" id="right-thigh" placeholder="Value" />
-        </div>
-        <div className="metrics-input-group">
-          <label htmlFor="left-thigh">Left Thigh</label>
-          <input type="number" id="left-thigh" placeholder="Value" />
-        </div>
-        <button type="submit" className="submit-button">Submit</button>
-      </form>
-    </div>
+      </div>
+      )}
+      </div>
   );
 };
 
